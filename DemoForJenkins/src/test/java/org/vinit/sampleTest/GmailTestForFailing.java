f47877416abde2c528b919b85308b86fef1faa2d
vinit4selenium <vinit4selenium@gmail.com>
2014-05-18 22:55:56 +0530

    This is for fail test

---
+++ DemoForJenkins/src/test/java/org/vinit/sampleTest/GmailTestForFailing.java
@@ -0,0 +1,19 @@
+package org.vinit.sampleTest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class GmailTestForFailing {
+
+	FirefoxDriver driver = new FirefoxDriver();
+	@Test
+	public void thisIsFirstTest(){
+		System.out.println("Before opening the ");
+		driver.get("https://www.google.co.in/");
+		driver.findElement(By.xpath("sdasdasd"));
+		driver.quit();
+		System.out.println("After closing the browser");
+		
+		
+	}	}
